afa7fe6699d8910ec21337294cdb8b7257995ff5
unknown <greshje.nachc@gmail.com>
2021-10-01 11:46:13 -0400

    added sam's stuff

--- validate.py
+++ validate.py
@@ -1,10 +1,16 @@
 from dcctools.config import Configuration
 
-c = Configuration("config.json")
-missing_files = c.validate_all_present()
-if len(missing_files) == 0:
-    print("All necessary input is present")
-else:
-    print("One or more files missing from data owners:")
-    for filename in missing_files:
-        print(filename)
+def validate():
+    c = Configuration("config.json")
+    missing_files = c.validate_all_present()
+    if len(missing_files) == 0:
+        print("All necessary input is present")
+        return "All necessary input is present"
+    else:
+        print("One or more files missing from data owners:")
+        for filename in missing_files:
+            print(filename)
+        return "One or more files missing from data owners"
+
+if __name__ == "__main__":
+    validate()
