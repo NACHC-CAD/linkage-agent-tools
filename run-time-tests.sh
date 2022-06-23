2724b616c0b8cce8cd6bf9b84e64933f5a01f734
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-06-23 09:25:09 -0400

    added nohub to run-time-tests.sh

--- run-time-tests.sh
+++ run-time-tests.sh
@@ -1,2 +1,2 @@
-python3 -u time_test.py --dir $1 | tee time-test.log
+nohup python3 -u time_test.py --dir $1 | tee time-test.log
 
--- runt-time-tests.sh
+++
@@ -1,1 +0,0 @@
-python3 -u time_test.py --dir $1 | tee time-test.log
