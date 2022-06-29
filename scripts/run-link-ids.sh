1315c621706c9ee04cffda059937a90e59ddd6f7
NACHC-CAD <69015612+NACHC-CAD@users.noreply.github.com>
2022-06-29 16:01:06 -0400

    updates to scripts

--- scripts/run-link-ids.sh
+++ scripts/run-link-ids.sh
@@ -1,1 +1,1 @@
-nohup python3 -u ../link_ids.py | tee ../logs/link-ids.log &
+nohup python3 -u link_ids.py | tee ./logs/link-ids.log &
--- scripts/run-time-tests.sh
+++ scripts/run-time-tests.sh
@@ -1,1 +1,1 @@
-nohup python3 -u time_test.py --dir $1 | tee ../logs/time-test.log &
+nohup python3 -u time_test.py --dir $1 | tee ./logs/time-test.log &
